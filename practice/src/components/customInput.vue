6f7d4282de800596b68183e3702ca9b7257e36bd
Dre'Sean Williams <dresean.l.williams@gmail.com>
2019-06-25 15:27:46 -0500

    add custom input event handling using v-model and $emit

---
+++ practice/src/components/customInput.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <input
+        :value='value'
+        @input='$emit("input", $event.target.value)'
+        placeholder='Please enter a phrase.'/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CustomInput',
+    props: ['value'],
+}
+</script>
+
+<style>
+
+</style>
+
+<!-- 
+
+-->
