d967ef0cbc7dfe185e3d8f19ae37b2a0393153f2
Dre'Sean Williams <dresean.l.williams@gmail.com>
2019-06-25 14:34:53 -0500

    add custom events with use of additional value parameters

---
+++ practice/src/components/comments.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <label>Title</label>
+        <p>{{post.title}}</p>
+        <br/>
+        <label>Body</label>
+        <p>{{post.body}}</p>
+        <br/>
+        <label>User Id</label>
+        <br/>
+        <p>{{post.userId}}</p>
+        <hr/>
+        <br/>
+        <button @click='$emit("enlarge-text", 0.1)'>Enlarge Text!</button>
+        <br/>
+        <button @click='$emit("decrease-text", 0.1)'>Decrease Text Size!</button>
+        <br/>
+        <br/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Comments',
+    props: ['post'],
+}
+</script>
+
+<style>
+
+</style>
